556c22c5ed3ca80d55b02642621ca8453b4faf68
Andrew Potter <apottere@gmail.com>
2014-09-19 14:53:28 +0000

    Initial commit.

---
+++ static/js/PokerController.js
@@ -0,0 +1,29 @@
+angular.module("app", ['ngAnimate'])
+	.controller("PokerController", function($scope) {
+
+		$scope.numbers = [ 0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ];
+		$scope.staged = false;
+		$scope.revealed = false;
+		$scope.selected = null;
+
+		$scope.onInit = function() {
+			window.addEventListener('shake', function() {
+				$scope.$apply(function() { if($scope.staged && !$scope.revealed) { $scope.revealClicked(); } });
+			}, false);
+		}
+
+		$scope.clickNumber = function(number) {
+			$scope.staged = true;
+			$scope.selected = number;
+		}
+
+		$scope.revealClicked = function() {
+			$scope.staged = false;
+			$scope.revealed = true;
+		}
+
+		$scope.clearClicked = function() {
+			$scope.revealed = false;
+		}
+	}
+);
