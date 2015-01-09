43700d54de0f1bcb0f19f496b35894cb9fce2afd
Andrew Potter <apottere@gmail.com>
2015-01-09 17:40:02 -0500

    Update whitespaces.

--- static/js/PokerController.js
+++ static/js/PokerController.js
@@ -1,51 +1,55 @@
 angular.module("app", ['ngAnimate'])
-	.run(function() {
-		FastClick.attach(document.body);
+    .run(function() {
+        FastClick.attach(document.body);
 
-		//create a new instance of shake.js.
-		var myShakeEvent = new Shake({
-			threshold: 14 // optional shake strength threshold
-		});
-		myShakeEvent && myShakeEvent.start();
-	})
-	.controller("PokerController", function($scope) {
+        //create a new instance of shake.js.
+        var myShakeEvent = new Shake({
+            threshold: 14 // optional shake strength threshold
+        });
+        myShakeEvent && myShakeEvent.start();
+    })
+    .controller("PokerController", function($scope) {
 
-		$scope.numbers = [ 0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ];
-		$scope.staged = false;
-		$scope.revealed = false;
-		$scope.selected = null;
+        $scope.numbers = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+        $scope.staged = false;
+        $scope.revealed = false;
+        $scope.selected = null;
 
-		$scope.onInit = function() {
-			window.addEventListener('shake', function() {
-				$scope.$apply(function() { if($scope.staged && !$scope.revealed) { $scope.revealClicked(); } });
-			}, false);
-		};
+        $scope.onInit = function() {
+            window.addEventListener('shake', function() {
+                $scope.$apply(function() {
+                    if($scope.staged && !$scope.revealed) {
+                        $scope.revealClicked();
+                    }
+                });
+            }, false);
+        };
 
-		$scope.clickNumber = function(number) {
-			$scope.staged = true;
-			$scope.selected = number;
-		};
+        $scope.clickNumber = function(number) {
+            $scope.staged = true;
+            $scope.selected = number;
+        };
 
-		$scope.revealClicked = function() {
-			$scope.staged = false;
-			$scope.revealed = true;
-		};
+        $scope.revealClicked = function() {
+            $scope.staged = false;
+            $scope.revealed = true;
+        };
 
-		$scope.clearClicked = function() {
-			$scope.revealed = false;
-		};
+        $scope.clearClicked = function() {
+            $scope.revealed = false;
+        };
 
-		$scope.backgroundClick = function() {
-			if($scope.staged && !$scope.revealed) {
-				$scope.revealClicked();
+        $scope.backgroundClick = function() {
+            if($scope.staged && !$scope.revealed) {
+                $scope.revealClicked();
 
-			} else if($scope.revealed && !$scope.staged) {
-				$scope.clearClicked();
-			}
-		};
+            } else if($scope.revealed && !$scope.staged) {
+                $scope.clearClicked();
+            }
+        };
 
-		$scope.noPropogate = function ($event) {
-			$event.stopPropagation();
-		}
-	}
+        $scope.noPropogate = function($event) {
+            $event.stopPropagation();
+        }
+    }
 );
